a30edffb9e3744b1c768ec49c9109fb1f6b5c038
WorkshopAPGH <yasamanshaykhan@gmail.com>
2019-03-07 10:53:23 +0330

    changed file main 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
         int x=4 ;
         int y = 8;
+        int dahlia=17;
 	    System.out.println(x+y);
+	    double c=3.2;
+	    int z=13;
     }
 }
