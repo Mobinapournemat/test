8d90229e2b74977abd5c1f483277b14a20695b9c
Mobina <mobina.glambert@gmail.com>
2019-03-07 10:49:20 +0330

    change change

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
         int x=4 ;
         int y = 8;
+        int z=0 ;
+                int d=0;
 	    System.out.println(x+y);
     }
 }
